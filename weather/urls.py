81ee6a319b31d2c0e8fd4484d1970f8056fe5e17
Erhan <erhantezer68@gmail.com>
2022-09-27 15:38:19 +0300

    updated

--- weather/urls.py
+++ weather/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from weather.views import index
+from weather.views import delete_city, index
 
 urlpatterns = [
-    path("", index, name="home")
+    path("", index, name="home"),
+    path("delete/<int:id>", delete_city, name="delete" ),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,64 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render,get_object_or_404, redirect
 from decouple import config
 import requests
 from pprint import pprint
 
+from weather.models import City
+
 # Create your views here.
 def index(request):
-    API_KEY=config("API_KEY")
+    API_KEY=config("API_KEY") #! api keyi env içinden config ile alıyoruz
     city = "Aksaray"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
-    response = requests.get(url)
-    content = response.json()
-    pprint(content)
-    pprint(content["name"])
-    pprint(content["main"]["temp"])
+    u_city = request.POST.get("name") #! formda gönderdiğimiz "name" yakalıyoruz
     
-    return render(request, "weather/index.html")
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
+        response =requests.get(url) #!url istek atıp cevap döndürdük
+        
+        if response.ok:
+            content = response.json() #! cevap varsa url den gelen verileri json yapısına çevir contente aktar
+            r_city = content["name"] #! content içinden  şehir adını al ve r_city aktar
+            
+            if City.objects.filter(name = r_city): #! şehir ismi database de varsa (City model sinde) 
+                messages.warning(request, "City already exists!")
+            else:
+                City.objects.create(name=r_city) #! yoksa City modelsi yardımıyla database kaydeder
+    
+    
+    #! database kaydedilen veri çekilip ihtiyaç olan kısımlar liste içine atılıp template içinde for döngüsüyle kullanacağız
+    city_data =[]
+    cities =City.objects.all()
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        content = response.json()
+        data = {
+            "city":city,
+            "temp" : content["main"]["temp"],
+            "icon" : content["weather"][0]["icon"],
+            "desc" : content["weather"][0]["description"]
+        }
+        city_data.append(data)
+        
+        context = {
+            "city_data":city_data
+        }
+    
+    
+    return render(request, "weather/index.html", context)
 
 
+def delete_city(request, id):
+    #* city = City.objects.get(id=id)
+    city = get_object_or_404(City, id=id)
+    city.delete()
+    messages.warning(request, "City deleted.")
+    return redirect("home")
+
+# pprint(content)
+    # pprint(content["name"])
+    # pprint(content["main"]["temp"])
 
 
 #! pprint sonucu aksaary için
